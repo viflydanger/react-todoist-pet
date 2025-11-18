40f79ee71d5880a883bbb513b5c095b7abf57875
viflydanger <vifly.danger@gmail.com>
2025-11-18 14:06:22 +0300

    first commit

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+const Header = () => {
+    return (
+    <header>
+                <h1>Welcome to the Header Component</h1>
+    </header>
+    );
+}
+
+export default Header;
